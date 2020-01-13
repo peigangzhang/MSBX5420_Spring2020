17f95ede177b40e328783fe430bbd4b8a96d933c
Peter Zhang <peizha@uber.com>
2020-01-13 14:58:46 -0700

    add word count python

---
+++ jupyter_notebooks/week1/word_count_python.py
@@ -0,0 +1,23 @@
+def get_word_counts(data_path):
+    """
+    return sorted (word, count) list, sort by count
+    """
+    word_count_dict = {}
+
+    with open(data_path, 'r') as f:
+        for line in f:
+            words = line.strip().split()
+            for word in words:
+                if word not in word_count_dict:
+                    word_count_dict[word] = 0
+                word_count_dict[word] += 1
+
+    sorted_word_counts = sorted(word_count_dict.items(), key=lambda kv: kv[1], reverse=True)
+    return sorted_word_counts
+
+
+input_data_path = './big_data_intro.txt'
+word_counts = get_word_counts(input_data_path)
+
+for word, count in word_counts[:100]:
+    print word, count
