052895e64ce7b207158b0bf00f9c2657a5d55552
Peter Zhang <peizha@uber.com>
2020-01-14 00:32:23 -0700

    fix word count pyspark file

--- lectures/week1/word_count_pyspark.py
+++ lectures/week1/word_count_pyspark.py
@@ -1,13 +1,17 @@
 from __future__ import print_function
 
-from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
-sc = SparkContext("local", "PythonWordCount")
+spark = SparkSession \
+    .builder \
+    .getOrCreate()
+
+sc = spark.sparkContext
 input_data_path = './big_data_intro.txt'
 text_file = sc.textFile(input_data_path)
+
 counts = text_file.flatMap(lambda line: line.split(" ")) \
     .map(lambda word: (word, 1)) \
     .reduceByKey(lambda a, b: a + b)
 
 print(counts.collect())
-sc.stop()
