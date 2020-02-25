4b1830ba18a05594dfc35cd3b9b5a317a7e11d20
Peter Zhang <peizha@uber.com>
2020-02-24 23:15:36 -0700

    week7 - add spark examples

--- lectures/week7/spark_example_hdfs_copy.py
+++ lectures/week7/spark_example_hdfs_copy.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 
-import argparse
 from pyspark.sql import SparkSession
 import pydoop.hdfs as pyhdfs
 import os
@@ -18,30 +17,15 @@ def copy_file(src_path, dst_path):
 
 
 """
-To run on Yarn
-spark-submit --master yarn --deploy-mode cluster --num-executors 2 --executor-memory 1G --executor-cores 1 --driver-memory 1G ./spark_example_hdfs_copy.py --src_dir hdfs://msbx5420-m/user/peter --dst_dir hdfs://msbx5420-m/test
-spark-submit --master yarn --deploy-mode cluster --num-executors 2 --executor-memory 1G --executor-cores 1 --driver-memory 1G ./spark_example_hdfs_copy.py --src_dir /user/peter --dst_dir /test
+To run on Yarn:
+spark-submit --master yarn --deploy-mode cluster --num-executors 2 --executor-memory 1G --executor-cores 1 --driver-memory 1G ./spark_example_hdfs_copy.py hdfs://msbx5420-m/user/peter hdfs://msbx5420-m/test
+
+To run on local mode:
+spark-submit --master local[2] ./spark_example_hdfs_copy.py hdfs://msbx5420-m/user/peter hdfs://msbx5420-m/test
 """
 
 
 def main():
-    """
-    parser = argparse.ArgumentParser(description='spark copy hdfs file')
-    parser.add_argument('--src_dir',
-                        required=True,
-                        type=str,
-                        help='source directory, e.g. hdfs://msbx5420-m/user/peter')
-
-    parser.add_argument('--dst_dir',
-                        required=True,
-                        type=str,
-                        help='destination directory, e.g. hdfs://msbx5420-m/test')
-
-    args = parser.parse_args()
-    src_dir = args.src_dir
-    dst_dir = args.dst_dir
-    """
-
     src_dir = str(sys.argv[1])
     dst_dir = str(sys.argv[2])
 
