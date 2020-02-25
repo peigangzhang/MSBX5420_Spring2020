e5385b613fc3c364fe733001e839f62d1a40d7b1
Peter Zhang <peizha@uber.com>
2020-02-24 23:00:59 -0700

    week7 - add spark examples

--- lectures/week7/spark_example_hdfs_copy.py
+++ lectures/week7/spark_example_hdfs_copy.py
@@ -4,7 +4,6 @@ import argparse
 from pyspark.sql import SparkSession
 import pydoop.hdfs as pyhdfs
 import os
-import sys
 
 
 def copy_file(src_path, dst_path):
@@ -16,13 +15,14 @@ def copy_file(src_path, dst_path):
     if not pyhdfs.path.exists(dst_path):
         pyhdfs.cp(src_path, dst_path)
 
+
 """
 To run on Yarn
-spark-submit --master yarn --deploy-mode cluster --num-executors 2 --executor-memory 1G --executor-cores 1 --driver-memory 1G ./spark_example_hdfs_copy.py --src_dir 'hdfs://msbx5420-m/user/peter' --dst_dir 'hdfs://msbx5420-m/test'
+spark-submit --master yarn --deploy-mode cluster --num-executors 2 --executor-memory 1G --executor-cores 1 --driver-memory 1G ./spark_example_hdfs_copy.py --src_dir hdfs://msbx5420-m/user/peter --dst_dir hdfs://msbx5420-m/test
 """
 
+
 def main():
-    """
     parser = argparse.ArgumentParser(description='spark copy hdfs file')
     parser.add_argument('--src_dir',
                         required=True,
@@ -37,11 +37,6 @@ def main():
     args = parser.parse_args()
     src_dir = args.src_dir
     dst_dir = args.dst_dir
-    """
-    src_dir = str(sys.argv[1])
-    dst_dir = str(sys.argv[2])
-    print(src_dir)
-    print(dst_dir)
 
     # create dst_dir if not exist
     if not pyhdfs.path.exists(dst_dir):
