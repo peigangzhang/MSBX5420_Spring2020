115ebbd1e5dd85d56d29fcc8e4e39442188713b6
Peter Zhang <peizha@uber.com>
2020-02-25 14:19:27 -0700

    week7 - add spark examples

--- lectures/week7/spark_example_monte_carlo_pi.py
+++ lectures/week7/spark_example_monte_carlo_pi.py
@@ -7,20 +7,27 @@ from operator import add
 from pyspark.sql import SparkSession
 
 
+def simulation():
+    """
+    get simulation result
+    :return:
+    """
+    x = random() * 2 - 1
+    y = random() * 2 - 1
+    return 1 if x ** 2 + y ** 2 <= 1 else 0
+
+
 def main():
     # create sparkcontext
     spark = SparkSession.builder.getOrCreate()
     sc = spark.sparkContext
 
+    # get partitions from input argument
     partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    # each partition simulate 100000 times
     n = 100000 * partitions
 
-    def f(_):
-        x = random() * 2 - 1
-        y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
-
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    count = spark.sparkContext.parallelize(range(n), partitions).map(simulation).reduce(add)
     print("Pi is roughly %f" % (4.0 * count / n))
 
     # stop sparkcontext
