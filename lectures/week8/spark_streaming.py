9b472e49763da8f154254e9b8e582505841b2788
peigangzhang <peigang.zhang@colorado.edu>
2020-03-03 00:24:59 -0700

    peer review demo

---
+++ lectures/week8/spark_streaming.py
@@ -0,0 +1,6 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+# Create a local StreamingContext with two working thread and batch interval of 1 second
+sc = SparkContext("local[2]", "NetworkWordCount")
+ssc = StreamingContext(sc, 1)
